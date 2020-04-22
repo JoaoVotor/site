9aec772d6ce71975497e686e43e5dbee193286de
JoaoVotor <62527513+JoaoVotor@users.noreply.github.com>
2020-04-22 20:03:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function doDemo (button) {
     var square = document.getElementById("square");
-    square.style.backgroundColor = "#fa4";
-    button.setAttribute("disabled", "true");
-    setTimeout(clearDemo, 2000, button);
+    for (var i=0; i<10;i++)
+    document.writeln('Aguardando')
   }
   
   function clearDemo (button) {
     var square = document.getElementById("square");
     square.style.backgroundColor = "transparent";
     button.removeAttribute("disabled");
+  }
+  function carrega(button){
+  var x = 0;
+  document.writeln('Espere 1 segundo ')
   }
