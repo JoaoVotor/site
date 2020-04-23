e763dab08863d75f6588c4e8e7bb5ffdc8448f0b
JoaoVotor <62527513+JoaoVotor@users.noreply.github.com>
2020-04-23 17:39:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function doDemo (button) {
-    var square = document.getElementById("square");
-    for (var i=0; i<10;i++)
-    document.writeln('Aguardando')
-  }
-  
-  function clearDemo (button) {
-    var square = document.getElementById("square");
-    square.style.backgroundColor = "transparent";
-    button.removeAttribute("disabled");
-  }
-  function carrega(button){
-  var x = 0;
-  document.writeln('Espere 1 segundo ')
-  }
+pass
