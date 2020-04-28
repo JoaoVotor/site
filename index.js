bffaa64d232db93a8616d0663db91401a91ee689
JoaoVotor <62527513+JoaoVotor@users.noreply.github.com>
2020-04-28 15:47:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const go = (elem) => {
-    window.scroll({       // 1
-        top: document
-      .querySelector( elem )
-        .offsetTop,       // 2
-        left: 0,
-        behavior: 'smooth'// 3
-     });
-  }
-  function soma(){
+
+function soma(){
     var txt1=window.document.getElementById('numero1')
     var txt2=window.document.getElementById('numero2')
     var n1 = Number(txt1.value)
